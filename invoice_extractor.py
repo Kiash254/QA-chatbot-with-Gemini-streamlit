ddb13eb357a83b81f4a7423b498f2fb8bab43ebc
Kiash254 <codewithkiash@gmail.com>
2023-12-27 19:37:24 +0300

    aded text files with chromadb and pyPDF2

--- invoice_extractor.py
+++ invoice_extractor.py
@@ -12,3 +12,34 @@ from PIL import Image
 
 #use configure to set the api key
 genai.configure(api_key=os.getenv("Google_api_key"))
+
+model=genai.GenerativeModel('gemini-pro-vision')
+
+def  get_gemini_response(input,image,prompt):
+    resesponse=model.generate_content([input,image[0],prompt])
+    return resesponse.text
+
+#streamlit set page config
+st.set_page_config(page_title="MultiLanguage Invoice Extractor",page_icon="🤖",layout="centered",initial_sidebar_state="auto")
+
+#set the header as Gemini pro application
+st.header("Gemini pro application")
+
+#set the input field
+input=st.text_input("input:",key="input")
+upload_file=st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
+
+image=""
+if upload_file is not None:
+    image=Image.open(upload_file)
+    st.image(image,caption="Uploaded Image",use_column_width=True)
+    
+    
+submit=st.button("Tell me about the invoice")
+
+input_prompt="This is an invoice for a purchase of a laptop. The invoice is for a laptop that was purchased on 12/12/2020. The invoice is for a laptop that was purchased on 12/12/2020. The invoice is for a laptop that was purchased on 12/12/2020. The invoice is for a laptop that was purchased on 12/12/2020. The invoice is for a laptop that was purchased on 12/12/2020. The invoice is for a laptop that was purchased on 12/12/2020. The invoice is for a laptop that was purchased on 12/12/2020. The invoice is for a laptop that was purchased on 12/12/2020. The invoice is for a laptop that was purchased on 12/12/2020."
+
+if submit:
+    response=get_gemini_response(input,[image],input_prompt)
+    st.subheader("The Response is:")
+    st.write(response)
