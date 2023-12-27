499036b63911e1ecaad23137c1bd08adc3b85a79
Kiash254 <codewithkiash@gmail.com>
2023-12-27 15:28:55 +0300

    updated readme file

--- vision.py
+++ vision.py
@@ -9,17 +9,14 @@ import google.generativeai as genai
 from PIL import Image
 #use configure to set the api key
 genai.configure(api_key=os.getenv("Google_api_key"))
-
 model=genai.GenerativeModel('gemini-pro-vision')
 #function to load gemini model and response
 def load_gemini_model(input,image):
     if input != "":
         response=model.generate_content([input,image])
-        
     else:
         response=model.generate_content(image)
     return response.text
-
 #set up the streamlit application
 
 #set the page_config as Upload an image  Bot with Gemini model and provide a description
@@ -37,8 +34,8 @@ upload_file=st.file_uploader("Choose an image....",type=['png','jpeg','jpg'])
 image=""
 
 if upload_file is not None:
-    image=upload_file.read()
-    st.image(upload_file,caption="Uploaded Image",use_column_width=True)
+    image=Image.open(upload_file)
+    st.image(upload_file,caption="Uploaded Image.",use_column_width=True)
     
     
 #create a submit button to tell about the image
