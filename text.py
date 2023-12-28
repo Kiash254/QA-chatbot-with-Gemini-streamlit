0a4f62e7d25f24969b7dddb98f4ae80cf81dfe96
Kiash254 <codewithkiash@gmail.com>
2023-12-28 21:50:56 +0300

    added new API related to gemini pro vision

--- text.py
+++ text.py
@@ -1,5 +1,7 @@
+import os
 import streamlit as st
 from transformers import pipeline
+from dotenv import load_dotenv
 
 def main():
     st.title('Text Generation with Hugging Face Transformers')
@@ -7,8 +9,11 @@ def main():
     st.sidebar.header('Instructions')
     st.sidebar.write('Please input a text and the model will generate a continuation of the text.')
 
+    load_dotenv()  # take environment variables from .env.
+    hugging_face_api_key = os.getenv("HUGGING_FACE_API")
+
     # Set up the model
-    pipe = pipeline("text2text-generation", model="google/flan-t5-large")
+    pipe = pipeline("text2text-generation", model="google/flan-t5-large", api_key=hugging_face_api_key)
 
     user_input = st.text_input("Input text:")
     if user_input:
