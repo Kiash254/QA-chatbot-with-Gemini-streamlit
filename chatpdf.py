f9d2c73de89c19c344fa77e74b76119da875e7ec
Kiash254 <codewithkiash@gmail.com>
2024-01-03 20:02:44 +0300

    added new mode of api and langchain chatting with multiple pdfs

---
+++ chatpdf.py
@@ -0,0 +1,15 @@
+#want to create a application that uses google gemini API to chat with multiple pdf at ones with streamlit
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+#load the env file
+load_dotenv()
